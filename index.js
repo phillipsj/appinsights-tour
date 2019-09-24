58507c436006b82b80d70b5e78ef69314abad9d6
Jamie Phillips <jamie@blueghostlabs.com>
2019-09-24 19:52:57 -0400

    I now have working example to demo.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load();
+    require('dotenv').config();
 }
 
 let appInsights = require('applicationinsights');
 appInsights.setup().start();
 
-const Azure = require("@azure/storage-blob");
+const azure = require('azure-storage');
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -16,8 +17,13 @@ app.get('/hello', (req, res) => res.send('Hello World!'));
 
 app.get('/error', (req, res) => { throw new Error("A demo error.")});
 
-app.get('/blob', (req, res) => {
-
+app.get('/dependencies', (req, res) => {
+    let blobService = azure.createBlobService();
+    blobService.doesBlobExist("images", "demo.jpg", function(error, result, response) {
+        if(!error) {
+            res.send('demo.jpg does exist');
+        }
+    });    
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
